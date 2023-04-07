adc4a6aa3640140a0b5668c52f461fc218acb9f9
Your Name <you@example.com>
2023-04-07 21:00:57 +0300

    Initial commit

---
+++ src/main/java/com/security/Security/newSecurConf.java
@@ -0,0 +1,44 @@
+package com.security.Security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class newSecurConf {
+    @Value("${spring.security.user.name}")
+    String userName;
+
+    @Value("${spring.security.user.password}")
+    String password;
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername(userName)
+                .password(passwordEncoder.encode(password))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests().anyRequest().hasRole("USER")
+                .and().formLogin().defaultSuccessUrl("/index", true);
+        return httpSecurity.build();
+    }
+
+    public  PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
+}
